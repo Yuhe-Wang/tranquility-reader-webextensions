1ec72693f72903100bc38856a90ad2eff43f5ab5
YUHE WANG <yuhewang.ustc@gmail.com>
2020-05-16 16:20:44 -0700

    Support finger tap to page up/down
    The code is referred from https://gitlab.com/rbauduin/ereaderify/

--- content_scripts/tranquilize.js
+++ content_scripts/tranquilize.js
@@ -243,6 +243,26 @@ function processResponse (oXHRDoc, thisURL, saveOffline) {
     processContentDoc(contentDoc, thisURL, saveOffline);
 }
 
+function addPageUpDownDiv(contentDoc) {
+    var style = "border:1px solid blue;position:fixed;top:0; z-index:1000;width:5vw;height:100%;opacity:0;";
+
+    var leftDiv = document.createElement("div");
+    leftDiv.setAttribute("id", "ereaderify-pgup");
+    leftDiv.setAttribute("class", "ereaderify-edge");
+    leftDiv.setAttribute("style", "left:0;" + style);
+    leftDiv.onclick = function () { window.scrollByPages(-1);};
+    console.log(leftDiv);
+    document.body.appendChild(leftDiv);
+
+    var rightDiv = document.createElement("div");
+    rightDiv.setAttribute("id", "ereaderify-pgup");
+    rightDiv.setAttribute("class", "ereaderify-edge");
+    rightDiv.setAttribute("style", "left:95%;" + style);
+    rightDiv.onclick = function () { window.scrollByPages(1);};
+    console.log(rightDiv);
+    contentDoc.body.appendChild(rightDiv);
+}
+
 function processContentDoc(contentDoc, thisURL, saveOffline) {
 
     // Remove all event handlers by cloning every single element
@@ -567,7 +587,8 @@ function processContentDoc(contentDoc, thisURL, saveOffline) {
     if (saveOffline) {
         saveContentOffline(thisURL, document.cloneNode(true));
     }
-    
+
+    addPageUpDownDiv(contentDoc)
 }
 
 function removeWhiteSpaceComments(cdoc) {
